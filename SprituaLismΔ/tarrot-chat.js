ac195a356c781d78a60a43907abe58b67945e714
AJ <dimezske@gmail.com>
2022-03-29 13:59:04 +1100

    Add files via upload

---
+++ SprituaLismΔ/tarrot-chat.js
@@ -0,0 +1,8 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("closebtn").style.display = "none";
+    window.location = "tarrot.html";
+  } 
